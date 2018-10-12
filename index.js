d115d800f00f041aa502e97d59a67a57df4f8a4f
jughead <jugheadeatsalot@gmail.com>
2018-10-11 20:39:42 -0700

    ...

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const normalizeLimit = exports.normalizeLimit = (reqLimit, max = 40, fallback = 30) => {
-    reqLimit = parseInt(reqLimit);
-
-    return (reqLimit > 0 && reqLimit <= max) ? reqLimit : fallback;
-};
+const bcrypt = require('bcryptjs');
 
 const merge = exports.merge = (object1, object2) => {
     for(const prop in object2) {
@@ -10,6 +6,36 @@ const merge = exports.merge = (object1, object2) => {
     }
 
     return object1;
+};
+
+const cleanObject = exports.cleanObject = (obj, safeKeys) => {
+    let output = {};
+
+    for(let i = 0; i < safeKeys.length; i++) {
+        if(obj.hasOwnProperty(safeKeys[i])) {
+            output[safeKeys[i]] = obj[safeKeys[i]];
+        }
+    }
+
+    return output;
+};
+
+const verifyPassword = exports.verifyPassword = (input, actual, next) => {
+    bcrypt.compare(input, actual, (err, isMatch) => {
+        if(err) return next(err);
+
+        return next(null, isMatch);
+    });
+};
+
+const verifyPasswordSync = exports.verifyPasswordSync = (input, hashed) => {
+    return bcrypt.compareSync(input, hashed);
+};
+
+const normalizeLimit = exports.normalizeLimit = (reqLimit, max = 40, fallback = 30) => {
+    reqLimit = parseInt(reqLimit);
+
+    return (reqLimit > 0 && reqLimit <= max) ? reqLimit : fallback;
 };
 
 const assembleQuery = exports.assembleQuery = (queryObject) => {
@@ -20,4 +46,12 @@ const assembleQuery = exports.assembleQuery = (queryObject) => {
     }
 
     return (queries) ? `?${queries.join('&')}` : '';
+};
+
+const oidOr = exports.oidOr = (value, altProperty = 'username') => {
+    if(/^[a-fA-F0-9]{24}$/.test(value)) {
+        return {$or: [{_id: value}, {[altProperty]: value}]};
+    } else {
+        return {[altProperty]: value};
+    }
 };
